4a4fad7684ee0c0f5d973db901c70a6fe17badda
arsaratovtsev <artem.saratovtsev@intel.com>
2020-09-21 17:06:49 +0300

    smth

--- modules/rgbd/src/tsdf_volume.hpp
+++ modules/rgbd/src/tsdf_volume.hpp
@@ -13,7 +13,34 @@ namespace cv
 namespace kinfu
 {
 
+class CV_EXPORTS_W NewVolume
+{
+public:
+    NewVolume(float _voxelSize, cv::Matx44f _pose, float _raycastStepFactor)
+        : voxelSize(_voxelSize),
+        voxelSizeInv(1.0f / voxelSize),
+        pose(_pose),
+        raycastStepFactor(_raycastStepFactor)
+    {
+    }
 
+    virtual ~NewVolume() {};
+
+    virtual void integrate(InputArray _depth, float depthFactor, const cv::Matx44f& cameraPose,
+        const cv::kinfu::Intr& intrinsics) = 0;
+    virtual void raycast(const cv::Matx44f& cameraPose, const cv::kinfu::Intr& intrinsics,
+        cv::Size frameSize, cv::OutputArray points,
+        cv::OutputArray normals) const = 0;
+    virtual void fetchNormals(cv::InputArray points, cv::OutputArray _normals) const = 0;
+    virtual void fetchPointsNormals(cv::OutputArray points, cv::OutputArray normals) const = 0;
+    virtual void reset() = 0;
+
+public:
+    const float voxelSize;
+    const float voxelSizeInv;
+    const cv::Affine3f pose;
+    const float raycastStepFactor;
+};
 
 }  // namespace kinfu
 }  // namespace cv
